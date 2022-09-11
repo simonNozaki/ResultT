360a95a18696d0b61f4d4b79bb45497b84065943
snozaki <snozaki@netprotections.co.jp>
2022-09-11 21:13:25 +0900

    ADD: error can state error name and status

--- src/error.ts
+++ src/error.ts
@@ -10,5 +10,7 @@ export class ValueNotFoundException extends Error {
    */
   constructor(message?: string) {
     super(message);
+    this.name = new.target.name;
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
